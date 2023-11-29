215996355098b6b4a42031a35d97e68cb8328579
Bruno Vercelli <bruno.vercelli@afya.com.br>
2023-11-29 17:22:13 -0300

    Create CLI with cobra lib

--- main.go
+++ main.go
@@ -1,20 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-
-	_ "github.com/mattn/go-sqlite3"
-
-	db2 "github.com/bjmvercelli/go_hexagonal/adapters/db"
-	"github.com/bjmvercelli/go_hexagonal/application"
-)
+import "github.com/bjmvercelli/go_hexagonal/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "db.sqlite")
-	productDbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-	product, _ := productService.Create("Product 1", 10.0)
-
-	productService.Enable(product)
-
+	cmd.Execute()
 }
