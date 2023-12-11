c24cef633b4f4311db3d78b542368cac4257f314
Bruno Vercelli <bruno.vercelli@afya.com.br>
2023-12-10 21:13:20 -0300

    create product route + dto

--- adapters/web/handler/product.go
+++ adapters/web/handler/product.go
@@ -4,13 +4,19 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/bjmvercelli/go_hexagonal/adapters/dto"
 	"github.com/bjmvercelli/go_hexagonal/application"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
 )
 
 func MakeProductHandlers(r *mux.Router, n *negroni.Negroni, service application.ProductServiceInterface) {
-	r.Handle("/product/{id}", n.With(negroni.Wrap(getProduct(service)))).Methods("GET", "OPTIONS")
+	r.Handle("/product/{id}", n.With(
+		negroni.Wrap(getProduct(service)),
+	)).Methods("GET", "OPTIONS")
+	r.Handle("/product", n.With(
+		negroni.Wrap(createProduct(service)),
+	)).Methods("POST")
 }
 
 func getProduct(service application.ProductServiceInterface) http.Handler {
@@ -28,6 +34,31 @@ func getProduct(service application.ProductServiceInterface) http.Handler {
 		err = json.NewEncoder(w).Encode(product)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	})
+}
+
+func createProduct(service application.ProductServiceInterface) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		var productDto dto.Product
+		err := json.NewDecoder(r.Body).Decode(&productDto)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(jsonError(err.Error()))
+			return
+		}
+		product, err := service.Create(productDto.Name, productDto.Price)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(jsonError(err.Error()))
+			return
+		}
+		err = json.NewEncoder(w).Encode(product)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(jsonError(err.Error()))
 			return
 		}
 	})
